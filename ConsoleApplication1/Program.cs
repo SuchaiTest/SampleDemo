8a806f034c493a813a5b10e59732b819f6190f0c
Suchai <Suchai.tammewar@optum.com>
2015-06-30 14:43:14 +0530

    more changes to index

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Sample Hello");
+            Console.WriteLine("SampleHI");
         }
     }
 }
